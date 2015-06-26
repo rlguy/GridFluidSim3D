1c93102e1872ebc771fedf92d7fb45a0af0cb805
rlguy <ryan.l.guy@gmail.com>
2015-06-25 21:54:44 -0700

    Added ImplicitScalarField object to project

---
+++ implicitsurfacescalarfield.cpp
@@ -0,0 +1,201 @@
+#include "implicitsurfacescalarfield.h"
+
+
+ImplicitSurfaceScalarField::ImplicitSurfaceScalarField() {
+}
+
+
+ImplicitSurfaceScalarField::ImplicitSurfaceScalarField(int i, int j, int k, double dx) :
+                                                       _isize(i), _jsize(j), _ksize(k), _dx(dx),
+                                                       _field(i, j, k, 0.0),
+                                                       _centerField(0, 0, 0, 0.0),
+                                                       _isVertexSolid(i, j, k, false) {
+}
+
+ImplicitSurfaceScalarField::~ImplicitSurfaceScalarField() {
+}
+
+void ImplicitSurfaceScalarField::clear() {
+    _field.fill(0.0);
+    _centerField.fill(0.0);
+}
+
+void ImplicitSurfaceScalarField::setPointRadius(double r) {
+    _radius = r;
+    _coef1 = (4.0 / 9.0)*(1.0 / (r*r*r*r*r*r));
+    _coef2 = (17.0 / 9.0)*(1.0 / (r*r*r*r));
+    _coef3 = (22.0 / 9.0)*(1.0 / (r*r));
+}
+
+void ImplicitSurfaceScalarField::enableCellCenterValues() {
+    if (_isCenterFieldEnabled) {
+        return;
+    }
+
+    _centerField = Array3d<double>(_isize-1, _jsize-1, _ksize-1, 0.0);
+
+    _isCenterFieldEnabled = true;
+}
+
+void ImplicitSurfaceScalarField::addPoint(glm::vec3 p, double r) {
+    setPointRadius(r);
+    addPoint(p);
+}
+
+void ImplicitSurfaceScalarField::addPoint(glm::vec3 p) {
+    GridIndex gmin, gmax;
+    _getGridIndexBounds(p, _radius, &gmin, &gmax);
+
+    glm::vec3 gpos;
+    glm::vec3 v;
+    double rsq = _radius*_radius;
+    double distsq;
+    double val;
+    for (int k = gmin.k; k <= gmax.k; k++) {
+        for (int j = gmin.j; j <= gmax.j; j++) {
+            for (int i = gmin.i; i <= gmax.i; i++) {
+                gpos = _GridIndexToPosition(i, j, k);
+                v = gpos - p;
+                distsq = glm::dot(v, v);
+                if (distsq < rsq) {
+                    val = _evaluateFieldFunctionForRadiusSquared(distsq);
+                    _field.add(i, j, k, val);
+                }
+
+                if (_isCenterFieldEnabled) {
+                    _calculateCenterCellValueForPoint(p, i, j, k);
+                }
+            }
+        }
+    }
+
+}
+
+void ImplicitSurfaceScalarField::addCuboid(glm::vec3 pos, double w, double h, double d) {
+    GridIndex gmin = _positionToGridIndex(pos);
+    GridIndex gmax = _positionToGridIndex(pos + glm::vec3(w, h, d));
+    AABB bbox = AABB(pos, w, h, d);
+
+    double eps = 10e-6;
+    glm::vec3 gpos;
+    for (int k = gmin.k; k <= gmax.k; k++) {
+        for (int j = gmin.j; j <= gmax.j; j++) {
+            for (int i = gmin.i; i <= gmax.i; i++) {
+                gpos = _GridIndexToPosition(i, j, k);
+                if (bbox.isPointInside(gpos)) {
+                    _field.add(i, j, k, _surfaceThreshold + eps);
+                }
+
+                if (_isCenterFieldEnabled) {
+                    _calculateCenterCellValueForCuboid(bbox, i, j, k);
+                }
+            }
+        }
+    }
+}
+
+void ImplicitSurfaceScalarField::setMaterialGrid(Array3d<int> &matGrid) {
+    assert(matGrid.width == _isize-1 && 
+           matGrid.height == _jsize-1 && 
+           matGrid.depth == _ksize-1);
+
+    GridIndex vertices[8];
+    for (int k = 0; k < _ksize-1; k++) {
+        for (int j = 0; j < _jsize-1; j++) {
+            for (int i = 0; i < _isize-1; i++) {
+                if (matGrid(i, j, k) == M_SOLID) {
+                    _getCellVertexIndices(i, j, k, vertices);
+                    for (int idx = 0; idx < 8; idx++) {
+                        _isVertexSolid.set(vertices[idx], true);
+                    }
+                }
+            }
+        }
+    }
+}
+
+void ImplicitSurfaceScalarField::getScalarField(Array3d<double> &field) {
+    assert(field.width == _field.width && 
+           field.height == _field.height && 
+           field.depth == _field.depth);
+
+    double val;
+    for (int k = 0; k < field.depth; k++) {
+        for (int j = 0; j < field.height; j++) {
+            for (int i = 0; i < field.width; i++) {
+                val = _field(i, j, k);
+                if (_isVertexSolid(i, j, k) && val > _surfaceThreshold) {
+                    val = _surfaceThreshold;
+                } 
+
+                field.set(i, j, k, val);
+            }
+        }
+    }
+}
+
+bool ImplicitSurfaceScalarField::isCellInsideSurface(int i, int j, int k) {
+    assert(_isCenterFieldEnabled);
+    assert(_centerField.isIndexInRange(i, j, k));
+
+    return _centerField(i, j, k) > _surfaceThreshold;
+}
+
+void ImplicitSurfaceScalarField::_getGridIndexBounds(glm::vec3 pos, double r, 
+                                                     GridIndex *gmin, GridIndex *gmax) {
+    GridIndex c = _positionToGridIndex(pos);
+    glm::vec3 cpos = _GridIndexToPosition(c);
+    glm::vec3 trans = pos - cpos;
+    double inv = 1.0 / _dx;
+
+    int imin = c.i - fmax(0, ceil((r-trans.x)*inv));
+    int jmin = c.j - fmax(0, ceil((r-trans.y)*inv));
+    int kmin = c.k - fmax(0, ceil((r-trans.z)*inv));
+    int imax = c.i + fmax(0, ceil((r-_dx+trans.x)*inv));
+    int jmax = c.j + fmax(0, ceil((r-_dx+trans.y)*inv));
+    int kmax = c.k + fmax(0, ceil((r-_dx+trans.z)*inv));
+
+    *gmin = GridIndex(fmax(imin, 0), fmax(jmin, 0), fmax(kmin, 0));
+    *gmax = GridIndex(fmin(imax, _isize-1), fmin(jmax, _jsize-1), fmin(kmax, _ksize-1));
+}
+
+double ImplicitSurfaceScalarField::_evaluateFieldFunctionForRadiusSquared(double rsq) {
+    return 1.0 - _coef1*rsq*rsq*rsq + _coef2*rsq*rsq - _coef3*rsq;
+}
+
+void ImplicitSurfaceScalarField::_getCellVertexIndices(int i, int j, int k, GridIndex vertices[8]){
+    vertices[0] = GridIndex(i,     j,     k);
+    vertices[1] = GridIndex(i + 1, j,     k);
+    vertices[2] = GridIndex(i + 1, j,     k + 1);
+    vertices[3] = GridIndex(i,     j,     k + 1);
+    vertices[4] = GridIndex(i,     j + 1, k);
+    vertices[5] = GridIndex(i + 1, j + 1, k);
+    vertices[6] = GridIndex(i + 1, j + 1, k + 1);
+    vertices[7] = GridIndex(i,     j + 1, k + 1);
+}
+
+void ImplicitSurfaceScalarField::_calculateCenterCellValueForPoint(glm::vec3 p, int i, int j, int k) {
+    if ( i == _isize - 1 || j == _jsize - 1 || k == _ksize - 1 ) {
+        return;
+    }
+
+    glm::vec3 gpos = _GridIndexToCellCenter(i, j, k);
+    glm::vec3 v = gpos - p;
+    double distsq = glm::dot(v, v);
+    if (distsq < _radius*_radius) {
+        double val = _evaluateFieldFunctionForRadiusSquared(distsq);
+        _centerField.add(i, j, k, val);
+    }
+}
+
+void ImplicitSurfaceScalarField::_calculateCenterCellValueForCuboid(AABB &bbox, int i, int j, int k) {
+    if ( i == _isize - 1 || j == _jsize - 1 || k == _ksize - 1 ) {
+        return;
+    }
+
+    glm::vec3 gpos = _GridIndexToCellCenter(i, j, k);
+    if (bbox.isPointInside(gpos)) {
+        double eps = 10e-6;
+        _centerField.add(i, j, k, _surfaceThreshold + eps);
+    }
+}
---
+++ implicitsurfacescalarfield.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+
+#include "glm/glm.hpp"
+#include "array3d.h"
+#include "aabb.h"
+
+class ImplicitSurfaceScalarField
+{
+public:
+    ImplicitSurfaceScalarField();
+    ImplicitSurfaceScalarField(int i, int j, int k, double dx);
+    ~ImplicitSurfaceScalarField();
+
+    void getGridDimensions(int *i, int *j, int *k) { *i = _isize; *j = _jsize; *k = _ksize; }
+    double getCellSize() { return _dx; }
+
+    void clear();
+    void setPointRadius(double r);
+    void enableCellCenterValues();
+    void addPoint(glm::vec3 pos, double radius);
+    void addPoint(glm::vec3 pos);
+    void addCuboid(glm::vec3 pos, double w, double h, double d);
+    void setSurfaceThreshold(double t) { _surfaceThreshold = t; }
+    double getSurfaceThreshold() { return _surfaceThreshold; }
+    void setMaterialGrid(Array3d<int> &matGrid);
+    void getScalarField(Array3d<double> &field);
+    bool isCellInsideSurface(int i, int j, int k);
+
+private:
+    inline GridIndex _positionToGridIndex(glm::vec3 p) {
+        double invdx = 1.0 / _dx;
+        return GridIndex(floor(p.x*invdx), floor(p.y*invdx), floor(p.z*invdx));
+    }
+
+    inline glm::vec3 _GridIndexToPosition(GridIndex g) {
+        return glm::vec3(g.i*_dx, g.j*_dx, g.k*_dx);
+    }
+
+    inline glm::vec3 _GridIndexToPosition(int i, int j, int k) {
+        return glm::vec3(i*_dx, j*_dx, k*_dx);
+    }
+
+    inline glm::vec3 _GridIndexToCellCenter(int i, int j, int k) {
+        return glm::vec3(i*_dx + 0.5*_dx, j*_dx + 0.5*_dx, k*_dx + 0.5*_dx);
+    }
+
+    void _getGridIndexBounds(glm::vec3 pos, double r, 
+                             GridIndex *gmin, GridIndex *gmax);
+
+    double _evaluateFieldFunctionForRadiusSquared(double rsq);
+    void _getCellVertexIndices(int i, int j, int k, GridIndex vertices[8]);
+
+    void _calculateCenterCellValueForPoint(glm::vec3 p, int i, int j, int k);
+    void _calculateCenterCellValueForCuboid(AABB &bbox, int i, int j, int k);
+
+    int M_SOLID = 2;
+
+    int _isize = 0;
+    int _jsize = 0;
+    int _ksize = 0;
+    double _dx = 0.0;
+
+    double _radius = 0.0;
+    double _coef1 = 0.0;
+    double _coef2 = 0.0;
+    double _coef3 = 0.0;
+
+    double _surfaceThreshold = 0.5;
+
+    Array3d<double> _field;
+    Array3d<double> _centerField;
+    Array3d<bool> _isVertexSolid;
+
+    bool _isCenterFieldEnabled = false;
+};
+
